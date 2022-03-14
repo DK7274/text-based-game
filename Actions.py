ccfd3592ad1ca6873e9df5495a8ecb64b48baf40
Darcy Keys <dkeys8722@gmail.com>
2022-03-14 19:31:03 +1100

    Fixed code for moving between rooms where it didnt check which room you moved to and so didnt change room description or allowed items, and so kept the description from previous rooms.

--- Actions.py
+++ Actions.py
@@ -8,6 +8,7 @@ def findAction(action):
             if Rooms.currentRoom < 5:
                 if Enemy.enemyCount == 0:
                     Rooms.currentRoom = Rooms.currentRoom + 1
+                    Rooms.roomChecker()
                     print("you move up a floor, you are now on floor " + str(Rooms.currentRoom))
                     print(Rooms.roomDesc)
                 else:
@@ -15,12 +16,16 @@ def findAction(action):
             else:
                 print("you are at the top floor, you cannot go any higher!")
         case "go down" | "down":
-            if Rooms.currentRoom > 0:
+            if Rooms.currentRoom > 1:
                 if Enemy.enemyCount == 0:
                     Rooms.currentRoom = Rooms.currentRoom - 1
+                    Rooms.roomChecker()
                     print("you move down a floor, you are now on floor " + str(Rooms.currentRoom))
                     print(Rooms.roomDesc)
                 else:
                     print("you cannot leave yet, there's more killing to do!")
             else:
                 print("you are on the bottom floor, you cannot go any lower")
+        case "interact":
+            print("you can interact with")
+            print( ','.join(Rooms.allowedInteract))
--- Rooms.py
+++ Rooms.py
@@ -1,25 +1,25 @@
 import Actions
 currentRoom = 1
 roomDesc = 1
-roomLight = 0
 allowedInteract = 1
 def roomOne():
     global roomDesc
     global allowedInteract
     roomDesc = "you are in a hotel lobby, with an elevator at the north end, and a reception desk with a small sign on it"
-    allowedInteract = ["sign","Sign","small sign"]
+    allowedInteract = ["Sign"]
 def roomTwo():
     global roomDesc
-    global roomLight
+    roomLight = 0
     global allowedInteract
     if roomLight == 0:
         roomDesc = "you are in a darkened room, there is a light switch on the wall beside the elevator. You can see a large rectangular object in the center of the room "
-        allowedInteract = ["light switch", "switch", "box"]
+        allowedInteract = ["Light Switch", "Box"]
 
     if roomLight == 1:
         roomDesc = "you are in a room with a dim bulb lighting it. Shadows flicker along the edges where the walls meet the floor. There is a green crate in the centre of the room."
-        allowedInteract = ["light switch", "switch", "crate", "green crate"]
+        allowedInteract = ["Light Switch", "Crate"]
 def roomChecker():
+    print("1")
     if currentRoom == 1:
         roomOne()
     elif currentRoom == 2:
